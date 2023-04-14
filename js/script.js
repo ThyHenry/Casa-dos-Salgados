bbd2c38d8fcd0ea9a07f5a8c6500c8e1cc0a4dd7
Thierry <thierryhenry130406@gmail.com>
2023-04-14 11:35:15 -0300

    first-version

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const Show = document.getElementById("hamburguer");
+const Nav_hide = document.getElementById("nav_hide");
+const Close = document.getElementById("close");
+
+Show.addEventListener("click", () => {
+    if(Nav_hide.style.display == "none" || !Nav_hide.style.display) {
+        Nav_hide.style.display = "block";
+    } else {
+        Nav_hide.style.display = "none";
+    }
+})
+
+Close.addEventListener("click", () => {
+    if(Nav_hide.style.display == "block" || !Nav_hide.style.display) {
+        Nav_hide.style.display = "none";
+    } else {
+        Nav_hide.style.display = "block";
+    }
+})
